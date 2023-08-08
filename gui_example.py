387862327ec6391175eefd14b6bc6df7e52c834a
Arthur Nangai <arthur.nangai@gmail.com>
2023-08-08 17:11:07 +0300

    Add event handling

--- gui_example.py
+++ gui_example.py
@@ -1,47 +1,51 @@
+import os
 import tkinter as tk
 import requests
+from dotenv import load_dotenv
+from functools import partial
+
+
+# Assumes you have created a .env file with SMTP_USER and SMTP_PASSWORD
+load_dotenv()  # take environment variables from .env.
+
+
+OPEN_WEATHER_API_KEY = os.environ.get('OPEN_WEATHER_API_KEY')
 
 
 def get_weather(api_query: str = 'Kampala,ug'):
-    url = f"api.openweathermap.org/data/2.5/weather?q={api_query}&APPID={{key}}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={api_query}&APPID={OPEN_WEATHER_API_KEY}"
     response = requests.request("GET", url, headers={}, data={})
     print(response.text)
     return None
 
 
-border_effects = {
-    "flat": tk.FLAT,
-    "sunken": tk.SUNKEN,
-    "raised": tk.RAISED,
-    "groove": tk.GROOVE,
-    "ridge": tk.RIDGE,
-}
+def handle_button_click(tk_frame: tk.Frame, event):
+    print('Event received, getting weather now')
+    lbl_weather_output = tk.Label(master=tk_frame, text='The weather is')
+    lbl_weather_output.pack()
+    return get_weather()
 
 
 window = tk.Tk()
-greeting = tk.Label(
-    text='Python rocks!',
-    foreground="red",
-    width=30,
-    height=15,
-)
-greeting.pack()
+frame = tk.Frame(master=window, relief=tk.SUNKEN, borderwidth=5)
+frame.pack()
+label_frame = tk.Label(master=frame, text='Weather Application. Click button to get weather', width=120)
+label_frame.pack()
+
+second_frame = tk.Frame(master=window, borderwidth=5)
+second_frame.pack()
 
 
 button = tk.Button(
-    text="Click here!",
+    master=second_frame,
+    text="Fetch Weather!",
     width=40,
-    height=15,
+    height=2,
     bg="blue",
     fg="white",
 )
 button.pack()
+button.bind("<Button-1>", partial(handle_button_click, second_frame))
 
-
-frame = tk.Frame(master=window, relief=border_effects['sunken'], borderwidth=5)
-frame.pack()
-label_frame = tk.Label(master=frame, text='Flat', width=120)
-label_frame.pack()
 
 window.mainloop()
-
